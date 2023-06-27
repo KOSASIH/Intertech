b5aae20f412ed1ef50eecaeabf05b41468d2dd7b
KOSASIH <kosasihg88@gmail.com>
2023-06-27 13:23:36 +0700

    Create preprocessing.py

---
+++ app/utilisasi/preprocessing.py
@@ -0,0 +1,5 @@
+def preprocess_data(data):
+    # Implement data preprocessing steps based on the requirements of the ML model
+    preprocessed_data = data  # Placeholder implementation
+    return preprocessed_data
+
