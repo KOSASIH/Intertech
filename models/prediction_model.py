9439c71ac6e8c2c23042c7fb66483273267eb908
KOSASIH <kosasihg88@gmail.com>
2023-06-27 13:17:10 +0700

    Create prediction_model.py

---
+++ models/prediction_model.py
@@ -0,0 +1,8 @@
+class PredictionModel:
+    def __init__(self, model_path):
+        # Load and initialize the ML model from the provided path
+        pass
+
+    def predict(self, data):
+        # Use the ML model to make predictions on the input data
+        pass
